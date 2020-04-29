c828d05a0b07fde094092496eecdea0e1c4588c9
Steven G. Johnson <stevenj@alum.mit.edu>
2020-04-29 16:40:54 -0400

    add a test for infinite limits

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,4 +20,13 @@ using Richardson, Test
     @test err < 1e-10
     @test all(x -> x < 0, X)
     @test length(X) == 6
+
+    empty!(X)
+    val, err = extrapolate(1.0, x0=Inf) do x
+        push!(X, x)
+        (x^2 + 3x - 2) / (x^2 + 5)
+    end
+    @test val ≈ 1 rtol=1e-12
+    @test err < 1e-10
+    @test length(X) == 7
 end
